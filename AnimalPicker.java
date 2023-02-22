a6d75e09686ca57758d04eafd0a1bd21fe5be601
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2023-02-21 19:46:56 -0500

    asked dog or cat

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 class AnimalPicker
 {
     public static void main(String[]args)
     {
-        
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Would you like to see a 'dog' or a 'cat' ?");
+        String input = in.nextLine();
+        if(input.toLowerCase().equals("dog"))
+        {
+            System.out.println("DOG ASCII ART HERE");
+        }
+        else if(input.toLowerCase().equals("cat"))
+        {
+            System.out.println("CAT ASCII ART HERE");
+        }
 
     }
 }
